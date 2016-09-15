1477cc7a39cbf737b898f653499c12e4983c304b
Ruby Menon <rmenon77@gmail.com>
2016-09-14 21:37:11 -1000

    coding saved

--- functions.js
+++ functions.js
@@ -45,19 +45,67 @@ function makeCrazyLongString(string){
 
 /*makeCrazyLongString('taco');
 Function - youGetTaco
-Write a function that takes a single String parameter called action. If the value passed into our function is eat have the function return the String value EAT TACOS.
+//Write a function that takes a single String parameter called action. 
+//If the value passed into our function is eat have the function 
+//return the String value EAT TACOS.
 */
+
+function youGetTaco(action) {
+  if (action === "eat") {
+      return "EAT TACOS";
+  
+  
+    } 
+  }
+  
+  console.log(youGetTaco ("eat"));
+
 
 
 
 /*Function - isNumberGreaterThan
-Write a function that takes two variables of type Number called first and second respectively. Return true if the first number is greater than the second.*/
+//Write a function that takes two variables of type Number called first and second respectively. Return true if the first number is greater than the second.*/
 
+/*function isNumberGreaterThan(first, second){
+  if (first>second) {
+    return true;
+  }
 
+  else {return false;
+
+  }
+
+}
+
+var verifyFunction = isNumberGreaterThan(20,10)
+console.log(verifyFunction)*/
+
+function isNumberGreaterThan(first, second) {
+  if (first>second) {
+    return true;
+  }
+
+}
+var verifyFunction = isNumberGreaterThan(20,10);
+console.log(verifyFunction);
+//alternate code without var is console.log(isNumberGreaterThan(5,1));
 
 /*Function - isTrue
 Write a function that takes in a Boolean value named val and have the function return whether the value is true.*/
 
+function isTrue(val) {
+  if (val === "cat") {
+    return true;
+
+  }
+  else {return false;
+  }
+
+  
+}
+var verifyIs = isTrue("pig");
+console.log(verifyIs);
+//without var: console.log(isTrue("cat"));
 
 
 
@@ -69,6 +117,19 @@ Write a function that takes in a Boolean value named val and have the function r
 
 /*Function - isEqual
 Write a function that takes two variables of type String called firstWord and secondWord respectively. Return 'AWWWWRIGHT' if the two are equal otherwise return 'Y U NO MATCH!'.*/
+
+function isEqual (firstWord, secondWord) {
+  if (firstWord === secondWord) {
+    return "AWWWWRIGHT"; 
+  }
+  else {return "Y U NO MATCH!";
+
+  }
+}
+
+var equal = isEqual("dog", "dog");
+console.log(equal);
+
 
 
 /*Function - isNotEqual
@@ -83,30 +144,117 @@ Write a function that takes two variables of type Boolean called first and secon
 /*Function - totalOver30
 Write a function that takes three variables of type Number called first and second and third respectively. Return true if the sum of all values are greater than 30 otherwise return false.*/
 
+function totalOver30 (first, second, third) {
+  if (first + second + third > 30) {
+    return true;
+
+  }
+  else {return false;
+
+  }
+
+}
+
+var total = totalOver30(10, 10, 5);
+console.log(total);
 
 
 /*Function - totalUnderWhat
 Write a function that takes four variables of type Number called first and second and third and fourth respectively. Return true if the sum of first, second and third are less than fourth otherwise return false.*/
 
+function totalUnderWhat (first, second, third, fourth){
+  if (first + second +third < fourth) {
+    return true;
+
+  } else {return false;
+
+  }
+
+}
+console.log(totalUnderWhat(1,5,10,15));
 
 
 /*Function - looptoNumber
 Write a function that takes in a Number value named limit and have the function write a for loop that loops the number of times of limit and console.log each number as the loop executes.*/
 
+// for (i = 0; i < 5; i++)
 
+function looptoNumber (limit) {
+  for (var i = 0; i<limit; i++) {
+    console.log(i);
+  }
+}
+
+looptoNumber(10);
 
 /*Function - showEachValue
 Write a function that takes in a Array value named characters that has a sequence of single character String values and have the function write a for loop that loops for each number of elements in the Array and console.log each character as the loop executes.*/
 
+/*The charAt() method returns the character at the specified index in a string.
 
+The index of the first character is 0, the second character is 1, and so on.
+
+Tip: The index of the last character in a string is string.length-1, the second last character is string.length-2, and so on
+*/
+
+/*var word = ["a", "b", "c"];
+
+function showEachValue(characters){
+ for(var i = 0; i<characters.length; i++){
+  console.log(characters[i]);
+ }
+}
+showEachValue(word);
+showEachValue("cats");*/
+
+var charStr = "dawgz"; 
+
+function showEachValue(characters) {
+    for (var i = 0; i < characters.length; i++) {
+      console.log(characters[i]);
+
+    }
+
+}
+showEachValue(charStr);
+  
 
 /*Function - createArrayFromString
 Write a function that takes a single variable of type String called word and write a for loop that creates an Array made up of each character in word except for "A" or "a". We don't want no stinking "A" or "a" in our Array. Note: You will need to use the Array.push() method to complete this function.*/
+
+/*function createArrayFromString (word) {
+  for (var i = 0; i<word.length; i++) {
+    0*/
+
 
 
 
 /*Function - greatSummator
 Write a function that takes an Array with any number of type Number and write a for loop to add all numbers in the Array and return the sum.*/
+
+/*var numberArray = [1,2,3,4,5];
+var total = 0;   //why do you have this var total set at 0?
+
+function greatSummator(arr){
+ for(var i = 0; i<arr.length; i++){
+  total +=arr[i]; // total = total + arr 
+ }
+ return total; //don't get how this adds up the array. there is nothing telling it to add
+
+}
+var addedUp = greatSummator(numberArray);
+console.log(addedUp);*/
+
+var sumArr = [1,2,3,4,5];
+
+function greatSummator(Array) {
+  for (var i = 0; i<Array.length; i++) {
+      
+
+  }
+
+}
+console.log(greatSummator[i]);
 
 
 
@@ -115,8 +263,5 @@ Write a function that takes an Array with any number of type Number and write a 
 Write a function that takes an Array with any number of type Number and second variable called total. Return true if the sum of all values in the Array are less than total otherwise return false.*/
 
 
-
-
 /*Function - checkTrueValues
 Write a function that takes an Array with any number of type Boolean values and write a for loop to call our isTrue function with each value as input and return true if all values return true from our isTrue function.*/
-
